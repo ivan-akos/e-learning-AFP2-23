8b64d7b650d8d64867b78eda86d9824b76f4b2bb
ivan-akos <114164620+ivan-akos@users.noreply.github.com>
2023-04-17 17:11:24 +0200

    views: added home view

--- src/django/elearning_app/views.py
+++ src/django/elearning_app/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Hello, world. Elearning index.")
+
+def home(request):
+    return render(request, 'home.html')    
