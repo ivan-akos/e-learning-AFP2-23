0d5af0dfd42d9d30560f26aba41aed1ac3a4e54b
ivan-akos <114164620+ivan-akos@users.noreply.github.com>
2023-04-18 15:45:17 +0200

    urls: added signup route

--- src/django/elearning_app/urls.py
+++ src/django/elearning_app/urls.py
@@ -6,3 +6,4 @@ urlpatterns = [
     path("", views.index, name="index"),
     path('home/', views.home, name='home'),
     path('courses/', views.courses, name='courses'),
+    path('signup', views.signup, name='signup'),
