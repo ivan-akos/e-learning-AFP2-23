e398850d97bd9f0cefaad0ebe79055fe5de2c50b
ivan-akos <114164620+ivan-akos@users.noreply.github.com>
2023-05-23 08:51:20 +0200

    urls: +profile

--- src/django/elearning_app/urls.py
+++ src/django/elearning_app/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [
+<<<<<<< HEAD
     re_path(r'^$', views.index, name="index"),
     re_path(r'^home/?$', views.home, name='home'),
     re_path(r'^courses/?$', views.courses, name='courses'),
@@ -12,6 +13,17 @@ urlpatterns = [
     re_path(r'^logout/?$', LogoutView.as_view(), name='logout'),
     re_path(r'^about/?$', views.about, name='about'),
     re_path(r'^contact/?$', views.contact, name='contact'),
+=======
+    path("", views.index, name="index"),
+    path('home/', views.home, name='home'),
+    path('courses/', views.courses, name='courses'),
+    path('profile/<int:user_id>/', views.profile, name='profile'),
+    path('signup', views.signup, name='signup'),
+    path('login', views.login, name='login'),
+	path('logout/', views.logout, name='logout'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
+>>>>>>> eabca9d9 (urls: +profile)
     path('course/<int:course_id>/',views.course, name='course'),
     re_path(r'^create_course/?$', views.create_course, name='create_course'),
     re_path(r'^login_wall/?$', views.login_wall, name='login_wall'),
