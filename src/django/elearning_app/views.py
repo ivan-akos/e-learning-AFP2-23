5f8d431270c65dec2bec7d553bb34be9b5aafe83
ivan-akos <114164620+ivan-akos@users.noreply.github.com>
2023-05-24 00:23:46 +0200

    views: create_course removed def

--- src/django/elearning_app/views.py
+++ src/django/elearning_app/views.py
@@ -71,9 +71,5 @@ def about(request):
 def contact(request):
     return render(request, 'contact.html')
 
-def create_course(request):
-    
-    return render(request, 'create_course.html')
-
 def login_wall(request):
     return render(request, 'login_wall.html')
